0cc9a23f6bee6f6d76332c24b0390178a369d078
emon3455 <emon.mhk69@gmail.com>
2023-02-03 00:54:48 +0600

    chepest price prom an object of phone

---
+++ 3problem.js
@@ -0,0 +1,27 @@
+const phones =[
+
+    {name: " I-Phone ",Price: 120000,color:"black"},
+    {name: " Xaomi ",  Price: 20000,color:"Silver"},
+    {name: " Oppo ",   Price: 40000,color:"Red"},
+    {name: " Realme ", Price: 30000,color:"Blue"},
+
+];
+
+
+function chepestPhone(phones){
+
+    let chep = phones[0];
+    for(let i = 0; i<phones.length; i++){
+
+        if(chep.Price > phones[i].Price ){
+            chep = phones[i];
+        }
+
+    }
+
+    return chep;
+
+}
+
+let chepPrice = chepestPhone(phones);
+console.log(chepPrice);
