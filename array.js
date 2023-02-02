7e67d0e7b72fdf1c6ac8fc6e9f328a9f85291368
emon3455 <emon.mhk69@gmail.com>
2023-02-02 22:51:52 +0600

    aray searching element and concating array

--- array.js
+++ array.js
@@ -1,2 +1,23 @@
 const arr = [10,20,30,40,50];
 
+console.log(Array.isArray(arr)); // it will return true if it is array
+
+// to check if a aray containing the element or not:
+console.log(arr.includes(50)); // return true if it is exist.
+
+
+// using index of to check an element of aray:
+
+if(arr.indexOf(30) !== -1){
+    console.log("Exist");
+}
+else{
+    console.log("Do not exist");
+}
+
+
+// to concatinate 2 array:
+const ary2 = [60,70,80,90];
+
+const newAray = arr.concat(ary2);
+console.log(newAray);
